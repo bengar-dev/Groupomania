1069d9075a3ff62bf58fe1d72856e87ae655e9f0
Benoit Garcia <benoitgar.pro@gmail.com>
2022-02-28 11:20:11 +0100

    fix: delete user

--- front/src/services/DeleteUser.js
+++ front/src/services/DeleteUser.js
@@ -3,10 +3,7 @@ import { api } from '../config/config.js'
 export function deleteUser(pass){
   const userInfo = JSON.parse(localStorage.getItem('info'))
   const axios = require('axios').default;
-  let data = {
-      password: pass
-  }
-
+  
   return axios.delete(api + '/api/user/' + userInfo.userId, {
     headers: {'Authorization' : 'Bearer ' + userInfo.token},
     data: {password: pass}
